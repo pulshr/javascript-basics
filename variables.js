a1fc73e0d846f6f840848657f7d3650b2b903230
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-08-25 15:29:41 +0530

    completed variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var person;
+var person = "John";
+console.log("Hello", person);
+
+var greeting = "Hello";
+console.log(greeting+" "+person);
+
+greeting = "Hi";
+person = "James";
+console.log(greeting+" "+person);
+
+/**
+ * Output
+ * - Hello John
+ * - Hello John
+ * - Hi James
+ **/
