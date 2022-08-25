fbe86c3babe6aa93a450ba9e2d80d6bf68a5452e
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-08-25 15:53:46 +0530

    arithmetic operators done

---
+++ arithmeticOperators.js
@@ -0,0 +1,21 @@
+console.log(2 + 2);
+console.log(1+2+3+4+5);
+console.log(20-18);
+console.log(2*3);
+console.log(8/1);
+console.log(3>2);
+console.log(2<3);
+console.log(10==10);
+
+/**
+ * 
+ * Output
+ *  - 4
+ *  - 15
+ *  - 2
+ *  - 6
+ *  - 8
+ *  - true
+ *  - true
+ *  - true
+ */
