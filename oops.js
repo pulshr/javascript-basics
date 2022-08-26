218e401d413c71a1dcd5f02c1acdf9231998bc12
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-08-26 20:50:09 +0530

    oops done

---
+++ oops.js
@@ -0,0 +1,16 @@
+//Store Manager Object
+var storeManager = {};
+storeManager.rangeTilesPerTurn = 4;
+storeManager.socialSkills = 50;
+storeManager.streetSmart = 50;
+storeManager.health = 30;
+storeManager.specialAbility = "finding business opportunities";
+storeManager.greeting = "Let's make some money";
+
+//Assistant Manager Object
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmart: 30,
+    health: 40
+};
